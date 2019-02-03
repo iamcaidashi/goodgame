152214f174ad394015ab76bd58bf549b653e40b6
caidashi <gohomeisi@163.com>
2019-02-03 09:27:07 +0800

    nb

---
+++ application/index/controller/Index.php
@@ -0,0 +1,16 @@
+<?php
+namespace app\index\controller;
+use think\Controller;
+use app\index\model\Hello;
+use app\common\model\Teacher;
+
+use think\Db;
+
+class Index extends Controller
+{
+    public function index()
+    {
+    }
+
+    
+}
---
+++ application/index/model/Hello.php
@@ -0,0 +1,12 @@
+<?php
+namespace app\index\model;
+use think\Model;
+class Hello extends Model{
+    
+    public function _initialize()
+    {
+        parent::_initialize();
+        
+    }
+   
+}
